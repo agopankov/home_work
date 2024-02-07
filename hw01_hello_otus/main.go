db78dd390b0d76873434ead669aef1a79aeed92e
aleksandr.gopankov <aleksandr.gopankov@gotbit.io>
2024-02-07 12:17:30 +0100

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,16 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
-	reversedString := reverse.String("Hello, OTUS!")
-
-	_, err := fmt.Fprintln(os.Stdout, reversedString)
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println(reverse.String("Hello, OTUS!"))
 }
