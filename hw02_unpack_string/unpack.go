b5d9c8530e07ff270e919c710b71d84bd51f095e
aleksandr.gopankov <aleksandr.gopankov@gotbit.io>
2024-02-27 13:15:33 +0100

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,44 @@
-package hw02unpackstring
+package hw02_unpack_string
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var builder strings.Builder
+	var prevRune rune
+	escapeMode := false
+
+	for i, r := range s {
+		if unicode.IsDigit(r) && !escapeMode {
+			if i == 0 || prevRune == 0 {
+				return "", ErrInvalidString
+			}
+			count, _ := strconv.Atoi(string(r))
+			if count > 0 {
+				builder.WriteString(strings.Repeat(string(prevRune), count))
+			}
+			prevRune = 0
+		} else {
+			if r == '\\' && !escapeMode {
+				escapeMode = true
+				continue
+			} else {
+				if prevRune != 0 {
+					builder.WriteRune(prevRune)
+				}
+				prevRune = r
+				escapeMode = false
+			}
+		}
+	}
+	if prevRune != 0 && !escapeMode {
+		builder.WriteRune(prevRune)
+	}
+	return builder.String(), nil
 }
